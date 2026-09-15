9929c43c07d4113edb3a8312afc33ee5eb8de396
Tumi Langemo <langemot@matc.edu>
2026-09-15 12:12:04 -0500

    Set up project

---
+++ server.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => res.send('API running'));
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
