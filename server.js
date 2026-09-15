0dacb6956c32e683c9db1e6ff1bab2193f015140
Tumi Langemo <langemot@matc.edu>
2026-09-15 12:30:34 -0500

    Added database connection

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import connectDatabase from './config/db.js';
 
 const app = express();
+
+connectDatabase();
 
 app.get('/', (req, res) => res.send('API running'));
 
