39f497c3f7fdfb3efcf81ed22a925d037f8855cb
Tumi Langemo <langemot@matc.edu>
2026-09-20 22:49:39 -0500

    Add validation to user registration

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+import { check, validationResult } from 'express-validator';
 import express from 'express';
 import connectDatabase from './config/db.js';
 
@@ -9,10 +10,21 @@ app.use(express.json());
 
 app.get('/', (req, res) => res.send('API running'));
 
-app.post('/api/users', (req, res) => {
-  console.log(req.body);
-  res.send(req.body);
-});
+app.post(
+  '/api/users',
+  [
+    check('name', 'Please enter your name').not().isEmpty(),
+    check('email', 'Please enter a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send(req.body);
+  }
+);
 
 const PORT = 3000;
 
